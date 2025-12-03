8d401653d18c6dd1e76d8ab3b3d90e714026338c
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-12-03 09:54:13 -0600

    se siguen modificando los estilos para que quede mejor visualmente

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,9 @@
+import image1 from '../utils/image1.jpg';
+import image2 from '../utils/image2.jpg';
+import image3 from '../utils/image3.jpg';
+import imagenJugador from "../utils/imageJugador.jpg";
+
+
 export default function Dashboard (){
     return(
         <div className="dashboard">
@@ -9,7 +15,7 @@ export default function Dashboard (){
 
                 <div className="dashboard__user">
 
-                    <img src="" alt="Foto de usuario" className="dashboard__user-img"/>
+                    <img src={imagenJugador} alt="Foto de usuario" className="dashboard__user-img"/>
                     <div>
                         <p className="dashboard__user-name">Nombre del usuario</p>
                         <a href="" className="dashboard__user-config">Configurar Perfil</a>
@@ -40,7 +46,11 @@ export default function Dashboard (){
                 
                 <div className="card">
                     <p className="card__title">Nombre del juego</p>
-                    <div className="card__img-box"></div>
+
+                    <div className="card__img-box">
+                        <img src={image1} alt="Juego" className="card__img" />
+                    </div>
+
                     <p className="card__console">PS5</p>
                     <p className="card__owned">Físico</p>
                     <div className="card__status">
@@ -48,6 +58,37 @@ export default function Dashboard (){
                     </div>
 
                 </div>
+
+                <div className="card">
+                    <p className="card__title">Nombre del juego</p>
+
+                    <div className="card__img-box">
+                        <img src={image2} alt="Juego" className="card__img" />
+                    </div>
+
+                    <p className="card__console">PS5</p>
+                    <p className="card__owned">Físico</p>
+                    <div className="card__status">
+                        <span className="dot playing"></span>
+                    </div>
+
+                </div>
+
+                <div className="card">
+                    <p className="card__title">Nombre del juego</p>
+
+                    <div className="card__img-box">
+                        <img src={image3} alt="Juego" className="card__img" />
+                    </div>
+
+                    <p className="card__console">PS5</p>
+                    <p className="card__owned">Físico</p>
+                    <div className="card__status">
+                        <span className="dot pending"></span>
+                    </div>
+
+                </div>
+
             </section>
         </div>
     );
